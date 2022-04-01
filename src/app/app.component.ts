05a36566f937b277115c2bcd87bd2043ad176911
SamuckqaDev <samuel.tomimatu@bandtec.com.br>
2022-04-01 09:54:40 -0300

    Photo component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app';
+  title = "AluraPic";
+
+  photos = [
+    {
+      url: "https://www.premierpet.com.br/wp-content/uploads/2020/10/banner-gato-3.jpg",
+      description: "gata"
+    },
+    {
+      url: "https://meupet.elanco.com/sites/g/files/adhwdz661/files/styles/paragraph_image/public/2020-03/gato-siames-sentado.jpg?itok=pUKQ_pPL", description: "gato"
+    },
+  ]
+
 }
